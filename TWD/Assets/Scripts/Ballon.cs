30201aa595ccd0a07aad0553d37d677309a6ab17
vcbenedet <93206103+vcbenedet@users.noreply.github.com>
2022-09-17 20:20:46 -0300

    Movimentação Balão

--- TWD/Assets/Scripts/Ballon.cs
+++ TWD/Assets/Scripts/Ballon.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ballon : MonoBehaviour
 {
+    
+    public Func<float, float> move_x = x => x + 0.05f;
+    public Func<float, float> move_y = x => 0.35f * MathF.Cos(x - 8);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +16,12 @@ public class Ballon : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+        gameObject.transform.position = new Vector3(move_x(transform.position.x), (move_y(transform.position.x)), 0);
     }
-  
+
+
+
 }
