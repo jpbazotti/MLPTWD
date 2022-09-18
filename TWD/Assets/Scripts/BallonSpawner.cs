1565b25ecb8a3777e2ec1d184e99b96e8fd453ff
vcbenedet <93206103+vcbenedet@users.noreply.github.com>
2022-09-18 16:48:18 -0300

    spawn balao

--- TWD/Assets/Scripts/BallonSpawner.cs
+++ TWD/Assets/Scripts/BallonSpawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BallonSpawner : MonoBehaviour
 {
+    public GameObject Ballon;
+    private float timer;
+    public float spawnrate;
+    public GameObject[] spawnpointers; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer = timer + Time.deltaTime;
+
+        if (timer >= spawnrate)
+        {
+            timer = 0;
+            Instantiate(Ballon, spawnpointers[0].transform);
+            Instantiate(Ballon, spawnpointers[1].transform);
+            Instantiate(Ballon, spawnpointers[2].transform);
+        }
     }
 }
