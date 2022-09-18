75c0099d129c954ea61487380c5e98d4cb40039e
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2022-09-18 19:19:50 -0300

    map now matches sin wave

--- TWD/Assets/Scripts/Ballon.cs
+++ TWD/Assets/Scripts/Ballon.cs
@@ -1,26 +1,45 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Ballon : MonoBehaviour
 {
     private bool pop;
-    public Func<float, float> move_x = x => x + 0.05f;
-    public Func<float, float> move_y = x => 0.35f * MathF.Cos(x - 8);
+    public Func<float, float> move_x_linear = x => x + 0.05f;
+    public Func<float, float> move_y_sin = x => MathF.Sin(x - 0.4f);
+    public Func<float, float> move_y_linear = x =>x;
 
+    int curPosition;
+    public IEnumerable<float> allPosX;
+    public IEnumerable<float> allPosY;
+    public Func<int, float, Func<float, float>, IEnumerable<float>> movementList()
+    {
+        Func<int, float, Func<float, float>, IEnumerable<float>> move = null;
+        move = (it, pos,function)=> it == 0 ? new List<float> {pos}.Concat(move(it + 1, function(pos), function)):
+                    it < 1000? new List<float> {pos}.Concat(move(it + 1, function(pos), function)) :
+                    new List<float> {};
+        return move;
+    }
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         pop = false;
+        curPosition = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        gameObject.transform.position = new Vector3(move_x(transform.position.x), (move_y(transform.position.x)), 0);
+        if (allPosX!=null && allPosY!=null)
+        {
+            gameObject.transform.position = new Vector3(allPosX.ElementAt(curPosition), allPosY.ElementAt(curPosition), gameObject.transform.position.z);
+            curPosition++;
+        }
     }
+   
+   
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
--- TWD/Assets/Scripts/BallonSpawner.cs
+++ TWD/Assets/Scripts/BallonSpawner.cs
@@ -1,16 +1,31 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BallonSpawner : MonoBehaviour
 {
-    public GameObject Ballon;
+    public GameObject ballon;
     private float timer;
     public float spawnrate;
-    public GameObject[] spawnpointers; 
+    public GameObject[] spawnpointers;
+    IEnumerable<float> xpath1;
+    IEnumerable<float> ypath1;
+    IEnumerable<float> xpath2;
+    IEnumerable<float> ypath2;
+    IEnumerable<float> xpath3;
+    IEnumerable<float> ypath3;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        Ballon script =ballon.GetComponent<Ballon>();
+        var f = script.movementList();
+        xpath1 = f(0, spawnpointers[0].transform.position.x, script.move_x_linear);
+        xpath2 = f(0, spawnpointers[1].transform.position.x, script.move_x_linear);
+        xpath3 = f(0, spawnpointers[2].transform.position.x, script.move_x_linear);
+
+        ypath1 = f(0, spawnpointers[0].transform.position.y, script.move_y_linear);
+        ypath2 = xpath2.Select(script.move_y_sin);
+        ypath3 = f(0, spawnpointers[2].transform.position.y, script.move_y_linear);
         timer = 0;
     }
 
@@ -22,9 +37,17 @@ public class BallonSpawner : MonoBehaviour
         if (timer >= spawnrate)
         {
             timer = 0;
-            Instantiate(Ballon, spawnpointers[0].transform.position,Quaternion.identity);
-            Instantiate(Ballon, spawnpointers[1].transform.position, Quaternion.identity);
-            Instantiate(Ballon, spawnpointers[2].transform.position, Quaternion.identity);
+            GameObject b1 =Instantiate(ballon, spawnpointers[0].transform.position,Quaternion.identity);
+            b1.GetComponent<Ballon>().allPosX = xpath1;
+            b1.GetComponent<Ballon>().allPosY = ypath1;
+
+            GameObject b2 = Instantiate(ballon, spawnpointers[1].transform.position, Quaternion.identity);
+            b2.GetComponent<Ballon>().allPosX = xpath2;
+            b2.GetComponent<Ballon>().allPosY = ypath2;
+
+            GameObject b3 = Instantiate(ballon, spawnpointers[2].transform.position, Quaternion.identity);
+            b3.GetComponent<Ballon>().allPosX = xpath3;
+            b3.GetComponent<Ballon>().allPosY = ypath3;
         }
     }
 }
